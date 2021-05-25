2c8b7dd9147ce35b2d5e71739992fe148cebca13
minhhuyena2cma@gmail.com <minhhuyena2cma@gmail.com>
2021-05-26 02:47:30 +0700

    Update ghep bai

--- ProductDetail.js
+++ ProductDetail.js
@@ -12,18 +12,3 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 5000);
 }
-$('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slider-nav'
-  });
-  $('.slider-nav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    dots: true,
-    centerMode: true,
-    focusOnSelect: true
-  });
